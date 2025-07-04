396f42d7f502de84b208818e62ba9c8623617015
WiczusWolf <oskar.zdrojewski@gmail.com>
2025-07-04 22:18:22 +0200

    Added custom event handler, Added event handling support for CMRA added tests

--- MyConsoleApp.Tests/CircularMultiResolutionArrayTests.cs
+++ MyConsoleApp.Tests/CircularMultiResolutionArrayTests.cs
@@ -38,4 +38,67 @@ public class CircularMultiResolutionArrayTests
 		}
 	}
 
+	[TestMethod]
+	public void HighResolutionCyclesWhenCapacityExceeded()
+	{
+		var arr = new CircularMultiResolutionArray<int>(3, 4, 2);
+		for (int i = 1; i <= 5; i++)
+		{
+			arr.PushFront(i);
+		}
+		Assert.AreEqual(3, arr.GetStartIndex(0));
+		Assert.AreEqual(5, arr[0, 0]);
+		Assert.AreEqual(2, arr[0, 3]);
+	}
+
+	[TestMethod]
+	public void MidResolutionCyclesWhenCapacityExceeded()
+	{
+		var arr = new CircularMultiResolutionArray<int>(3, 4, 2);
+		for (int i = 1; i <= 10; i++)
+		{
+			arr.PushFront(i);
+		}
+		Assert.AreEqual(3, arr.GetStartIndex(1));
+		Assert.AreEqual(9, arr[1, 0]);
+		Assert.AreEqual(3, arr[1, 3]);
+	}
+
+	[TestMethod]
+	public void LowestResolutionCyclesWhenCapacityExceeded()
+	{
+		var arr = new CircularMultiResolutionArray<int>(3, 4, 2);
+		for (int i = 1; i <= 20; i++)
+		{
+			arr.PushFront(i);
+		}
+		Assert.AreEqual(3, arr.GetStartIndex(2));
+		Assert.AreEqual(18, arr[2, 0]);
+		Assert.AreEqual(6, arr[2, 3]);
+	}
+
+	[TestMethod]
+	public void OnItemAddedIsInvokedWithAddedValue()
+	{
+		var arr = new CircularMultiResolutionArray<int>(1, 2, 2);
+		int? added = null;
+		arr.OnItemAdded.Add(i => added = i);
+		arr.PushFront(42);
+		Assert.AreEqual(42, added);
+	}
+
+	[TestMethod]
+	public void OnItemRemovedIsInvokedWhenItemIsOverwritten()
+	{
+		var arr = new CircularMultiResolutionArray<int>(1, 2, 2);
+		int? removed = null;
+
+		arr.OnItemAdded.Add(i => removed = i);
+
+		arr.PushFront(1);
+		arr.PushFront(2);
+		arr.PushFront(3); // overwrites 1
+		Assert.AreEqual(1, removed);
+	}
+
 }
--- MyConsoleApp/CircularMultiResolutionArray.cs
+++ MyConsoleApp/CircularMultiResolutionArray.cs
@@ -1,89 +1,124 @@
-using System;
+using Common.Delegates;
 using System.Numerics;
 
 namespace MyConsoleApp
 {
-    public class CircularMultiResolutionArray<T> where T : INumber<T>
-    {
-        private readonly int _partitions;
-        private readonly int _size;
-        private readonly int _increase;
-        private readonly T[][] _arrays;
-        private readonly int[] _starts;
-        private readonly T[] _sums;
-        private readonly int[] _counts;
+	public class CircularMultiResolutionArray<T> where T : INumber<T>
+	{
+		private readonly int _partitions;
+		private readonly int _size;
+		private readonly int _increase;
+		private readonly T[][] _arrays;
+		private readonly int[] _starts;
+		private readonly T[] _sums;
+		private readonly int[] _counts;
+		private int _filledCount;
 
-        public int Partitions => _partitions;
-        public int Size => _size;
-        public int Increase => _increase;
+		public readonly EventHandlerSync<T> OnItemAdded = new();
+		public readonly EventHandlerSync<T> OnItemRemoved = new();
 
-        public CircularMultiResolutionArray(int partitions, int size, int increase)
-        {
-            if (partitions <= 0) throw new ArgumentOutOfRangeException(nameof(partitions));
-            if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size));
-            if (increase <= 0) throw new ArgumentOutOfRangeException(nameof(increase));
+		public int Partitions => _partitions;
+		public int Size => _size;
+		public int Increase => _increase;
 
-            _partitions = partitions;
-            _size = size;
-            _increase = increase;
-            _arrays = new T[partitions][];
-            _starts = new int[partitions];
-            _sums = new T[partitions];
-            _counts = new int[partitions];
+		public CircularMultiResolutionArray(int partitions, int size, int increase)
+		{
+			if (partitions <= 0) throw new ArgumentOutOfRangeException(nameof(partitions));
+			if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size));
+			if (increase <= 0) throw new ArgumentOutOfRangeException(nameof(increase));
 
-            for (int i = 0; i < partitions; i++)
-            {
-                _arrays[i] = new T[size];
-                _starts[i] = 0;
-                _sums[i] = T.Zero;
-                _counts[i] = 0;
-            }
-        }
+			_partitions = partitions;
+			_size = size;
+			_increase = increase;
+			_arrays = new T[partitions][];
+			_starts = new int[partitions];
+			_sums = new T[partitions];
+			_counts = new int[partitions];
+			_filledCount = 0;
 
-        public void PushFront(T item)
-        {
-            PushToLevel(0, item);
-        }
+			for (int i = 0; i < partitions; i++)
+			{
+				_arrays[i] = new T[size];
+				_starts[i] = 0;
+				_sums[i] = T.Zero;
+				_counts[i] = 0;
+			}
+		}
 
-        private void PushToLevel(int level, T item)
-        {
-            if (level >= _partitions)
-                return;
+		public void PushFront(T item)
+		{
+			int start = (_starts[0] - 1 + _size) % _size;
+			bool removed = false;
+			T removedValue = _arrays[0][start];
+			if (_filledCount == _size)
+			{
+				removed = true;
+			}
+			else
+			{
+				_filledCount++;
+			}
 
-            int start = (_starts[level] - 1 + _size) % _size;
-            _starts[level] = start;
-            _arrays[level][start] = item;
+			_starts[0] = start;
+			_arrays[0][start] = item;
 
-            _sums[level] += item;
-            _counts[level]++;
+			_sums[0] += item;
+			_counts[0]++;
 
-            if (_counts[level] >= _increase)
-            {
-                T avg = _sums[level] / T.CreateChecked(_increase);
-                _sums[level] = T.Zero;
-                _counts[level] = 0;
-                PushToLevel(level + 1, avg);
-            }
-        }
+			if (_counts[0] >= _increase)
+			{
+				T avg = _sums[0] / T.CreateChecked(_increase);
+				_sums[0] = T.Zero;
+				_counts[0] = 0;
+				PushToLevel(1, avg);
+			}
 
-        public T this[int partition, int index]
-        {
-            get
-            {
-                if (partition < 0 || partition >= _partitions)
-                    throw new ArgumentOutOfRangeException(nameof(partition));
-                if (index < 0 || index >= _size)
-                    throw new ArgumentOutOfRangeException(nameof(index));
-                int realIndex = (_starts[partition] + index) % _size;
-                return _arrays[partition][realIndex];
-            }
-        }
+			OnItemAdded?.Invoke(item);
+			if (removed)
+			{
+				OnItemRemoved?.Invoke(removedValue);
+			}
+		}
 
-        public int GetStartIndex(int partition)
-        {
-            if (partition < 0 || partition >= _partitions)
-                throw new ArgumentOutOfRangeException(nameof(partition));
-            return _starts[partition];
-        }
-    }
+		private void PushToLevel(int level, T item)
+		{
+			if (level >= _partitions)
+				return;
+
+			int start = (_starts[level] - 1 + _size) % _size;
+			_starts[level] = start;
+			_arrays[level][start] = item;
+
+			_sums[level] += item;
+			_counts[level]++;
+
+			if (_counts[level] >= _increase)
+			{
+				T avg = _sums[level] / T.CreateChecked(_increase);
+				_sums[level] = T.Zero;
+				_counts[level] = 0;
+				PushToLevel(level + 1, avg);
+			}
+		}
+
+		public T this[int partition, int index]
+		{
+			get
+			{
+				if (partition < 0 || partition >= _partitions)
+					throw new ArgumentOutOfRangeException(nameof(partition));
+				if (index < 0 || index >= _size)
+					throw new ArgumentOutOfRangeException(nameof(index));
+				int realIndex = (_starts[partition] + index) % _size;
+				return _arrays[partition][realIndex];
+			}
+		}
+
+		public int GetStartIndex(int partition)
+		{
+			if (partition < 0 || partition >= _partitions)
+				throw new ArgumentOutOfRangeException(nameof(partition));
+			return _starts[partition];
+		}
+	}
 }
