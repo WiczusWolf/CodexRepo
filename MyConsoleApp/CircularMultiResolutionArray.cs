81208ce3985f233ad3273d5e76573ad59e140eec
WiczusWolf <oskar.zdrojewski@gmail.com>
2025-07-05 15:38:18 +0200

    Add naive index access and index struct

--- MyConsoleApp/CircularMultiResolutionArray.cs
+++ MyConsoleApp/CircularMultiResolutionArray.cs
@@ -21,6 +21,50 @@ namespace MyConsoleApp
         public int Size => _size;
         public int Increase => _increase;
 
+        public readonly struct IndexInfo
+        {
+            public IndexInfo(int partition, int partitionIndex, int offset)
+            {
+                Partition = partition;
+                PartitionIndex = partitionIndex;
+                Offset = offset;
+            }
+
+            public int Partition { get; }
+            public int PartitionIndex { get; }
+            public int Offset { get; }
+        }
+
+        private int Pow(int exponent)
+        {
+            int result = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                result *= _increase;
+            }
+            return result;
+        }
+
+        public IndexInfo GetIndex(int naiveIndex)
+        {
+            if (naiveIndex <= 0)
+                throw new ArgumentOutOfRangeException(nameof(naiveIndex));
+
+            int idx = naiveIndex - 1;
+            for (int p = 0; p < _partitions; p++)
+            {
+                int factor = Pow(p);
+                int partitionIndex = idx / factor;
+                if (partitionIndex < _size)
+                {
+                    int offset = idx % factor;
+                    return new IndexInfo(p, partitionIndex, offset);
+                }
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(naiveIndex));
+        }
+
         public CircularMultiResolutionArray(int partitions, int size, int increase)
         {
             if (partitions <= 0) throw new ArgumentOutOfRangeException(nameof(partitions));
@@ -104,6 +148,29 @@ namespace MyConsoleApp
                     throw new ArgumentOutOfRangeException(nameof(index));
                 int realIndex = (_starts[partition] + index) % _size;
                 return _arrays[partition][realIndex];
+            }
+        }
+
+        public T this[int naiveIndex]
+        {
+            get
+            {
+                var info = GetIndex(naiveIndex);
+                return this[info];
+            }
+        }
+
+        public T this[IndexInfo index]
+        {
+            get
+            {
+                if (index.Partition < 0 || index.Partition >= _partitions)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                if (index.PartitionIndex < 0 || index.PartitionIndex >= _size)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+
+                int realIndex = (_starts[index.Partition] + index.PartitionIndex) % _size;
+                return _arrays[index.Partition][realIndex];
             }
         }
 
